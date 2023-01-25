63d35f0e5de3121ac7d6f214ddd93d1bad4b21da
Skip Fowler <skiplfowler@hotmail.com>
2023-01-25 08:15:56 -0700

    feat: Hello world window

--- projects/immediate_ui_experiment/main.cpp
+++ projects/immediate_ui_experiment/main.cpp
@@ -1,7 +1,37 @@
 #include <GLFW/glfw3.h>
 #include <spdlog/spdlog.h>
 
-auto main() -> int 
-{ 
-  spdlog::info("Hello World!"); 
+GLFWwindow* NewWindow(const int width, const int height, const char* title) {
+  if (!glfwInit()) {
+    spdlog::error("Unable to initialize GLFW");
+    exit(-1);
+  }
+
+  return glfwCreateWindow(width, height, title, NULL, NULL);
+}
+
+void Shutdown() { glfwTerminate(); }
+
+auto main() -> int {
+  constexpr int kWindowWidth = 640;
+  constexpr int kWindowHeight = 480;
+  const char* kWindowTitle = "Hello world!";
+
+  GLFWwindow* window = NewWindow(kWindowWidth, kWindowHeight, kWindowTitle);
+
+  if (nullptr == window) {
+    spdlog::error("Unable to create window");
+    Shutdown();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  Shutdown();
 }
